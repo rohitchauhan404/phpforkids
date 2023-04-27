5e8194200d4db632904238cfe7b882de15448b7e
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2023-04-27 12:53:59 +0530

    Add files via upload

---
+++ evenodd.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+
+<?php
+
+$num = readline('Enter number: ' . PHP_EOL);
+$status = null;
+
+if($num % 2 == 0) {
+    $status = 'even';
+    } else {
+    $status = 'odd';
+    }
+
+echo 'The number ' . $num . ' is ' . $status . PHP_EOL;
+
+?>
