fb54f4c2ad5ba8fc363ff5dd99a3e4e1b0d2b9ed
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2023-04-27 16:31:45 +0530

    Add files via upload
    armstrong number closures

---
+++ armstrong.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+
+<?php
+
+$num = readline('Enter number: ' . PHP_EOL);
+$status = ' is not Armstrong';
+
+$numarray = str_split($num);
+$power = count($numarray);
+
+$sum = array_sum(array_map(function($item) use ($power) {
+    return pow($item, $power);
+}
+, $numarray));
+
+if ($num == $sum ) {
+    $status = ' is Armstrong';
+    }
+
+echo 'The number ' . $num . $status . PHP_EOL;
+
+?>
