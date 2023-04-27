091829aba3ddcc31b030a8eedce249888b4261c1
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2023-04-27 13:01:25 +0530

    Add files via upload
    palindrome

---
+++ palindrome.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+
+<?php
+
+$num = readline('Enter number: ' . PHP_EOL);
+$status = null;
+
+if (strrev($num) == $num){ 
+    $status = 'is'; 
+    } else {
+    $status = 'is not';
+    }
+
+echo 'The number ' . $status . ' a palindrome ' . PHP_EOL;
+
+?>
