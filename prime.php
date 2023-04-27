cb7c622c88ee5229fbed92f68ccb86b912d4f2f9
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2023-04-27 13:13:01 +0530

    Add files via upload
    prime

---
+++ prime.php
@@ -0,0 +1,21 @@
+#!/usr/bin/php
+
+<?php
+
+$num = readline('Enter number: ' . PHP_EOL);
+$status = ' is prime';
+
+if ($num == 1) {
+    $status = ' is not prime';
+    }
+
+for ($i = 2; $i <= $num/2; $i++) {
+    if ($num % $i == 0) {
+    $status = ' is not prime';
+    break;
+    }
+    }
+
+echo 'The number ' . $num . $status . PHP_EOL;
+
+?>
