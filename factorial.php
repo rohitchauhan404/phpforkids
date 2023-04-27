bf34f89ffe0a9c12195ce2f8fad721996ff4820e
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2023-04-27 12:49:39 +0530

    Add files via upload
    factorial

---
+++ factorial.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+
+<?php
+
+$num = readline('Enter factorial number: ' . PHP_EOL);
+
+function factorial($num) {
+    if($num <= 1) {
+    return 1;
+    } else {
+    return $num * factorial($num-1);
+    }
+}
+
+echo 'Factorial of ' . $num . ' is ' . factorial($num) . PHP_EOL;
+
+?>
