7ea1a47698bb0529847894ad0f0183d8af52eca6
ROHIT CHAUHAN <rohitchauhan404@outlook.com>
2023-04-28 12:27:43 +0530

    array_filter with closure
    array_filter with closure

---
+++ filtereven.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+
+<?php
+
+$numbers = readline('Enter numbers to filter even numbers (comma separated list): ' . PHP_EOL);
+$numbers = explode(',', $numbers);
+
+$filteredeven = array_filter($numbers, function($item) {
+    return $item % 2 == 0;
+});
+echo 'These are the even numbers: ' . implode(',', $filteredeven) . PHP_EOL;
+
+?>
